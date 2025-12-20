b0966df0610ff2021df0101ea52b0b28b18f9de6
ayazpatel <mo.ayazpatel305@gmail.com>
2025-12-20 10:26:47 +0530

    SSL Certificate

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://Swasemi:YfJgkGrtHhTBexnO@cluster0.iwnypay.mongodb.net/rain_logger?retryWrites=true&w=majority");
     console.log("✅ MongoDB Atlas connected");
   } catch (err) {
     console.error("❌ MongoDB error:", err.message);
--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import logRoute from "./routes/logRoute.js";
+import fs from 'fs';
+import https from 'https'
+
 
 dotenv.config();
 connectDB();
@@ -39,7 +42,12 @@ app.get("/", (req, res) => {
   res.send("Rain Logger API Running");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+const options = {
+  key: fs.readFileSync('../localhost-key.pem'),
+  cert: fs.readFileSync('../localhost.pem'),
+};
+
+
+https.createServer(options, app).listen(5050, () => {
+  console.log("https server running at https://localhost:5050")
+})
